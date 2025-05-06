45598440a8b8da433558ae5b7ebdab8bb8fb53d6
matthew <matthew@mccollom.info>
2025-05-05 22:48:38 -0500

    Fixed null reference exception Added document controller

--- src/Server/Controllers/DocumentController.cs
+++ src/Server/Controllers/DocumentController.cs
@@ -1,9 +1,49 @@
+using HsaLedger.Application.Mediator.Commands;
+using HsaLedger.Application.Mediator.Queries;
+using HsaLedger.Application.Requests;
+using HsaLedger.Application.Responses.Projections;
+using HsaLedger.Shared.Wrapper;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace HsaLedger.Server.Controllers;
 
 [Authorize]
 public class DocumentController : ApiControllerBase
 {
+    [HttpGet]
+    [Authorize(Roles = "Administrator,Operations")]
+    public async Task<Result<IEnumerable<DocumentResponse>>> Get()
+    {
+        var query = new GetDocumentQuery();
+        var result = await Mediator.Send(query);
+        return result;
+    }
     
+    [HttpPut]
+    [Authorize(Roles = "Administrator,Operations")]
+    public async Task<Result<int>> Add(AddDocumentRequest document)
+    {
+        var command = new AddDocumentCommand(document);
+        var result = await Mediator.Send(command);
+        return result;
+    }
+    
+    [HttpDelete]
+    [Route("{documentId::int}"), Authorize(Roles = "Administrator,Operations")]
+    public async Task<Result<int>> Delete(int documentId)
+    {
+        var command = new DeleteDocumentCommand(documentId);
+        var result = await Mediator.Send(command);
+        return result;
+    }
+    
+    [HttpPost]
+    [Authorize(Roles = "Administrator,Operations")]
+    public async Task<Result<int>> Set(SetDocumentRequest document)
+    {
+        var command = new SetDocumentCommand(document);
+        var result = await Mediator.Send(command);
+        return result;
+    }
 }
